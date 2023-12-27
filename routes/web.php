2e197d48900c2ea3c26526a65b77ecbcbe0b6b7d
Rizki Nuzul Muzaki <rizki.nm01@gmail.com>
2023-12-27 19:52:57 +0700

    feat: prepare controller, model, route, middleware

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('role:admin')->group(function () {
+    return view('admin');
+});
+
 require __DIR__.'/auth.php';
